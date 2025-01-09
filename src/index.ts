c0af8c9ac37f660942c41c0386acdecd8efa053d
AbdelrhmanAmin <abdo.amin9991@gmail.com>
2025-01-10 01:39:05 +0200

    move examples out of src

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import useFetch from "./useFetch";
+import useFetchConfig from "./useFetchConfig";
+import { FetchProvider } from "./providers/ContextProvider";
+
+export { useFetch, useFetchConfig, FetchProvider };
