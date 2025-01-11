0a308b32d79943779160b564b34ba46ccf5f6c8c
AbdelrhmanAmin <abdo.amin9991@gmail.com>
2025-01-11 17:47:44 +0200

    test: enhance useFetch tests to verify additional properties in the returned object

--- tests/useFetch.test.tsx
+++ tests/useFetch.test.tsx
@@ -11,10 +11,14 @@ describe("useFetch", () => {
       "[useFetch] key must be a non-empty string."
     );
   });
-  it("returns an object with data", () => {
+  it("returns an object with data, error, isLoading, fetchData, revalidate", () => {
     const { result } = renderHook(() =>
       useFetch("https://jsonplaceholder.typicode.com/todos/1")
     );
     expect(result.current).toHaveProperty("data");
+    expect(result.current).toHaveProperty("error");
+    expect(result.current).toHaveProperty("isLoading");
+    expect(result.current).toHaveProperty("fetchData");
+    expect(result.current).toHaveProperty("revalidate");
   });
 });
